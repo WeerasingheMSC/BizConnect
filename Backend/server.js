6bb934f5c589c87977bb1f1de04629643f899975
WeerasingheMSC <sahanchampathi2001ugc@gmail.com>
2025-12-13 13:51:15 +0530

    Create connection with DB

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './src/config/db.js';
+
 
 dotenv.config();
 
@@ -8,8 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+// Connect to MongoDB
+connectDB();
 
+//connecting server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
